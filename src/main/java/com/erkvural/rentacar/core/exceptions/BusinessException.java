937204b44bc0cda3fd405c5a7ca54dcacc2271dd
Erk-Vural <erk.vural@gmail.com>
2022-02-17 22:53:18 +0300

    0.0.9 - created core/exceptions ( BusinessException )

---
+++ src/main/java/com/erkvural/rentacar/core/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.erkvural.rentacar.core.exceptions;
+
+public class BusinessException extends Exception {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
