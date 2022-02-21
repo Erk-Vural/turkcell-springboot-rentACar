84010cc75581a3cfc6bd3bb8ecf2613408fad3c9
Erk-Vural <er.bugrahan@gmail.com>
2022-02-21 11:53:08 +0300

    0.1.7 - Created core/utilities/results (DataResult,SuccessResult,SuccessDataResult,ErrorResult,ErrorDataResult,Result)

---
+++ src/main/java/com/erkvural/rentacar/core/utilities/results/ErrorDataResult.java
@@ -0,0 +1,20 @@
+package com.erkvural.rentacar.core.utilities.results;
+
+public class ErrorDataResult<T> extends DataResult<T> {
+
+    public ErrorDataResult(T data) {
+        super(false, data);
+    }
+
+    public ErrorDataResult(String message, T data) {
+        super(false, message, data);
+    }
+
+    public ErrorDataResult(String message) {
+        super(false, message, null);
+    }
+
+    public ErrorDataResult() {
+        super(false, null);
+    }
+}
---
+++ src/main/java/com/erkvural/rentacar/core/utilities/results/SuccessDataResult.java
@@ -0,0 +1,20 @@
+package com.erkvural.rentacar.core.utilities.results;
+
+public class SuccessDataResult<T> extends DataResult<T> {
+
+    public SuccessDataResult(T data) {
+        super(true, data);
+    }
+
+    public SuccessDataResult(String message, T data) {
+        super(true, message, data);
+    }
+
+    public SuccessDataResult(String message) {
+        super(true, message, null);
+    }
+
+    public SuccessDataResult() {
+        super(true, null);
+    }
+}
